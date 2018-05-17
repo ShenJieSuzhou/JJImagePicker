10c6b9885af0f0c651013a841691a6458b38b4dc
ShenJieSuzhou <745689122@qq.com>
2018-05-17 11:34:20 +0800

    新建工程 提交
    新建工程 提交

---
+++ JJImagePickerDemo/JJImagePickerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JJImagePickerDemo
+//
+//  Created by shenjie on 2018/5/17.
+//  Copyright © 2018年 shenjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JJImagePickerDemo/JJImagePickerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JJImagePickerDemo
+//
+//  Created by shenjie on 2018/5/17.
+//  Copyright © 2018年 shenjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
