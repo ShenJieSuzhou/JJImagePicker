0931c0abb3d31d89aa29bae9ae730831493bfa8d
ShenJieSuzhou <745689122@qq.com>
2018-06-19 14:21:25 +0800

    解决预览图滚动时速度太快的效果
    解决预览图滚动时速度太快的效果

--- JJImagePickerDemo/JJImagePickerDemo/ImagePicker/Views/CameraRoll/SubViews/JJPreviewViewCollectionLayout.h
+++ JJImagePickerDemo/JJImagePickerDemo/ImagePicker/Views/CameraRoll/SubViews/JJPreviewViewCollectionLayout.h
@@ -10,4 +10,18 @@
 
 @interface JJPreviewViewCollectionLayout : UICollectionViewFlowLayout
 
+//翻页的滚动速度,超过该速度就强制翻页。默认速度为 0.4
+@property (nonatomic, assign) CGFloat velocityForPageDown;
+
+//是否支持可以滑动多个 item
+@property (nonatomic, assign) BOOL allowMultipleItemScroll;
+
+/*
+ * 当支持一次滑动允许滚动多个 item 的时候，滑动速度要达到多少才会滚动多个 item，默认为 0.7
+ * allowMultipleItemScroll 为 YES 时生效
+ */
+@property(nonatomic, assign) CGFloat multipleItemScrollVelocityLimit;
+
+
+
 @end
--- JJImagePickerDemo/JJImagePickerDemo/ImagePicker/Views/CameraRoll/SubViews/JJPreviewViewCollectionLayout.m
+++ JJImagePickerDemo/JJImagePickerDemo/ImagePicker/Views/CameraRoll/SubViews/JJPreviewViewCollectionLayout.m
@@ -12,6 +12,9 @@
 
 - (instancetype)init{
     if(self = [super init]){
+        self.velocityForPageDown = 0.4;
+        self.allowMultipleItemScroll = YES;
+        
         self.collectionView.pagingEnabled = YES;
         self.minimumLineSpacing = 0;
         self.minimumInteritemSpacing = 0;
@@ -26,6 +29,21 @@
     CGFloat itemH = self.collectionView.frame.size.height;
     
     self.itemSize = CGSizeMake(itemW, itemH);
+}
+
+- (CGPoint)targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset withScrollingVelocity:(CGPoint)velocity{
+    CGFloat itemSpacing = self.itemSize.width + self.minimumLineSpacing;
+    
+    if(self.allowMultipleItemScroll || fabs(velocity.x) <= fabs(self.multipleItemScrollVelocityLimit)){
+        BOOL scrollingToRight = proposedContentOffset.x < self.collectionView.contentOffset.x;
+        proposedContentOffset = CGPointMake(self.collectionView.contentOffset.x + (itemSpacing / 2) * (scrollingToRight ? -1:1), self.collectionView.contentOffset.y);
+    }else{
+        proposedContentOffset = self.collectionView.contentOffset;
+    }
+    
+    proposedContentOffset.x = round(proposedContentOffset.x / itemSpacing) * itemSpacing;
+    
+    return proposedContentOffset;
 }
 
 @end
