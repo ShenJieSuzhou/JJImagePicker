f27b89af99fa49f690bd62384c59ea471ee15757
ShenJieSuzhou <745689122@qq.com>
2018-10-18 19:48:30 +0800

    新增涂抹模型对象
    涂抹模型对象

---
+++ JJImagePickerDemo/JJImagePickerDemo/ImagePicker/Model/ScrawlModel.h
@@ -0,0 +1,17 @@
+//
+//  ScrawlModel.h
+//  JJImagePickerDemo
+//
+//  Created by shenjie on 2018/10/18.
+//  Copyright © 2018年 shenjie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ScrawlModel : NSObject
+
+@property (strong, nonatomic) NSString *patternName;
+
+@end
+
+
---
+++ JJImagePickerDemo/JJImagePickerDemo/ImagePicker/Model/ScrawlModel.m
@@ -0,0 +1,29 @@
+//
+//  ScrawlModel.m
+//  JJImagePickerDemo
+//
+//  Created by shenjie on 2018/10/18.
+//  Copyright © 2018年 shenjie. All rights reserved.
+//
+
+#import "ScrawlModel.h"
+
+@implementation ScrawlModel
+
+
+//- (instancetype)initWithName:(NSString *)name{
+//    self = [super init];
+//    if(self){
+//        self.pa = name;
+//        self.point = CGPointMake(0, 0);
+//        self.dircetion = TAG_DIRECTION_LEFT;
+//    }
+//    
+//    return self;
+//}
+//
+//- (void)updateTagName:(NSString *)tagName{
+//    self.tagName = tagName;
+//}
+
+@end
