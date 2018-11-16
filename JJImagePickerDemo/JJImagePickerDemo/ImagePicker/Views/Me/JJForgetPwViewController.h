8aece6e18694bf83f856b0f5aef9c62e30fe38a6
ShenJieSuzhou <745689122@qq.com>
2018-11-16 14:10:12 +0800

    账号密码登录 + 忘记密码
    账号密码登录 + 忘记密码

---
+++ JJImagePickerDemo/JJImagePickerDemo/ImagePicker/Views/Me/JJForgetPwViewController.h
@@ -0,0 +1,13 @@
+//
+//  JJForgetPwViewController.h
+//  JJImagePickerDemo
+//
+//  Created by silicon on 2018/11/16.
+//  Copyright © 2018年 shenjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JJForgetPwViewController : UIViewController
+
+@end
---
+++ JJImagePickerDemo/JJImagePickerDemo/ImagePicker/Views/Me/JJZMLoginViewController.m
@@ -0,0 +1,37 @@
+//
+//  JJZMLoginViewController.m
+//  JJImagePickerDemo
+//
+//  Created by silicon on 2018/11/16.
+//  Copyright © 2018年 shenjie. All rights reserved.
+//
+
+#import "JJZMLoginViewController.h"
+
+@interface JJZMLoginViewController ()
+
+@end
+
+@implementation JJZMLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
