1a8baeedb0565c861faaa1c9daa5a33035f6f7e5
ShenJieSuzhou <745689122@qq.com>
2019-02-14 23:17:08 +0800

    七牛上传图片创建token逻辑
    七牛上传图片创建token逻辑

--- JJImagePickerDemo/JJImagePickerDemo/Upload/JJImageUploadManager.m
+++ JJImagePickerDemo/JJImagePickerDemo/Upload/JJImageUploadManager.m
@@ -8,6 +8,20 @@
 
 #import "JJImageUploadManager.h"
 #import <Qiniu/QiniuSDK.h>
+#import <Qiniu/QN_GTM_Base64.h>
+#include <CommonCrypto/CommonDigest.h>
+#include <CommonCrypto/CommonHMAC.h>
+#import<QNConfiguration.h>
+
+@interface JJImageUploadManager()
+
+@property (nonatomic, copy) NSString *accessKey;
+@property (nonatomic, copy) NSString *secretKey;
+@property (nonatomic, copy) NSString *scope;
+@property (assign) NSInteger liveTime;
+@property (nonatomic, copy) NSString *uploadToken;
+
+@end
 
 @implementation JJImageUploadManager
 
@@ -16,21 +30,111 @@
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         instance = [[JJImageUploadManager alloc] init];
+        instance.accessKey = @"DdEttx2bOBYCjubF-hhZhclOzE6iEW9xm24L7G8I";
+        instance.secretKey = @"ib5YZR-YhwuavoRxpgg6tWSXeERHQ6tzPt_5EY9o";
+        instance.scope = @"beautyImage";
+        instance.liveTime = 100;
     });
     
     return instance;
 }
 
 - (void)uploadImageToQN:(NSString *)imageUrl image:(UIImage *)image{
-    NSString *token = @"从服务端SDK获取";
-    QNUploadManager *upManager = [[QNUploadManager alloc] init];
-    NSData *data = [@"Hello, World!" dataUsingEncoding : NSUTF8StringEncoding];
-    [upManager putData:data key:@"hello" token:token
-              complete: ^(QNResponseInfo *info, NSString *key, NSDictionary *resp) {
-                  NSLog(@"%@", info);
-                  NSLog(@"%@", resp);
-              } option:nil];
+//    NSString *token = @"从服务端SDK获取";
+//    QNUploadManager *upManager = [[QNUploadManager alloc] init];
+//    NSData *data = [@"Hello, World!" dataUsingEncoding : NSUTF8StringEncoding];
+//    [upManager putData:data key:@"hello" token:token
+//              complete: ^(QNResponseInfo *info, NSString *key, NSDictionary *resp) {
+//                  NSLog(@"%@", info);
+//                  NSLog(@"%@", resp);
+//              } option:nil];
 }
 
+
+- (void)createToken {
+    if (!self.scope.length || !self.accessKey.length || !self.secretKey.length) {
+        return ;
+    }
+    
+    // 将上传策略中的scrop和deadline序列化成json格式
+    NSMutableDictionary *authInfo = [NSMutableDictionary dictionary];
+    [authInfo setObject:self.scope forKey:@"scope"];
+    [authInfo
+     setObject:[NSNumber numberWithLong:[[NSDate date] timeIntervalSince1970] + self.liveTime * 24 * 3600]
+     forKey:@"deadline"];
+    
+    NSData *jsonData =
+    [NSJSONSerialization dataWithJSONObject:authInfo options:NSJSONWritingPrettyPrinted error:nil];
+    
+    // 对json序列化后的上传策略进行URL安全的base64编码
+    NSString *encodedString = [self urlSafeBase64Encode:jsonData];
+    
+    // 用secretKey对编码后的上传策略进行HMAC-SHA1加密，并且做安全的base64编码，得到encoded_signed
+    NSString *encodedSignedString = [self HMACSHA1:self.secretKey text:encodedString];
+    
+    // 将accessKey、encodedSignedString和encodedString拼接，中间用：分开，就是上传的token
+    NSString *token = [NSString stringWithFormat:@"%@:%@:%@", self.accessKey, encodedSignedString, encodedString];
+    
+    self.uploadToken = token;
+}
+
+
+- (NSString *)urlSafeBase64Encode:(NSData *)text {
+    NSString *base64 =
+    [[NSString alloc] initWithData:[QN_GTM_Base64 encodeData:text] encoding:NSUTF8StringEncoding];
+    base64 = [base64 stringByReplacingOccurrencesOfString:@"+" withString:@"-"];
+    base64 = [base64 stringByReplacingOccurrencesOfString:@"/" withString:@"_"];
+    return base64;
+}
+
+- (NSString *)HMACSHA1:(NSString *)key text:(NSString *)text {
+    const char *cKey = [key cStringUsingEncoding:NSUTF8StringEncoding];
+    const char *cData = [text cStringUsingEncoding:NSUTF8StringEncoding];
+    
+    char cHMAC[CC_SHA1_DIGEST_LENGTH];
+    
+    CCHmac(kCCHmacAlgSHA1, cKey, strlen(cKey), cData, strlen(cData), cHMAC);
+    
+    NSData *HMAC = [[NSData alloc] initWithBytes:cHMAC length:CC_SHA1_DIGEST_LENGTH];
+    NSString *hash = [self urlSafeBase64Encode:HMAC];
+    return hash;
+}
+
++(void)uploadImageToQNFilePath:(NSArray *)photos success:(QNSuccessBlock)success failure:(QNFailureBlock)failure{
+    
+//    NSMutableArray *imageAry =[NSMutableArray new];
+//    NSMutableArray *imageAdd = [NSMutableArray new];
+//    for (ZLPhotoAssets *status in photos) {
+//        [imageAry addObject:[status aspectRatioImage]];
+//    }
+//    //主要是把图片或者文件转成nsdata类型就可以了
+//    QNConfiguration *config = [QNConfiguration build:^(QNConfigurationBuilder *builder) {
+//        builder.zone = [QNZone zone0];}];
+//    QNUploadManager *upManager = [[QNUploadManager alloc] initWithConfiguration:config];
+//    QNUploadOption *uploadOption = [[QNUploadOption alloc] initWithMime:nil
+//                                                        progressHandler:^(NSString *key, float percent) {
+//                                                            NSLog(@"上传进度 %.2f", percent);
+//                                                        }
+//                                                                 params:nil
+//                                                               checkCrc:NO
+//                                                     cancellationSignal:nil];
+//    [imageAry enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//        NSLog(@"%ld",idx);
+//        NSData *data;
+//        if (UIImagePNGRepresentation(obj) == nil){
+//            data = UIImageJPEGRepresentation(obj, 1);
+//        } else {
+//            data = UIImagePNGRepresentation(obj);}
+//        [upManager putData:data key:[QiniuLoad qnImageFilePatName] token:[QiniuLoad makeToken:accessKey secretKey:secretKey] complete:^(QNResponseInfo *info, NSString *key, NSDictionary *resp) {
+//            NSLog(@"%@",resp[@"key"]);
+//            if (info.isOK) {
+//                [imageAdd addObject:[NSString stringWithFormat:@"%@%@",kQNinterface,resp[@"key"]]];}else{
+//                    [imageAdd addObject:[NSString stringWithFormat:@"%ld",idx]];}
+//            if (imageAdd.count == imageAry.count) {
+//                if (success) {
+//                    success([imageAdd componentsJoinedByString:@";"]);}}}
+//                    option:uploadOption];}];
+    
+}
 
 @end
