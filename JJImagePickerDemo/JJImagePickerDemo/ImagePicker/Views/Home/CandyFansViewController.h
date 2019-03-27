ad9e78c0d5599a923dab20d6b6f52d17d53bfdd2
ShenJieSuzhou <745689122@qq.com>
2019-03-27 10:43:47 +0800

    新建其他用户主页以及粉丝主页
    新建其他用户主页以及粉丝主页

---
+++ JJImagePickerDemo/JJImagePickerDemo/ImagePicker/Views/Home/CandyFansViewController.h
@@ -0,0 +1,17 @@
+//
+//  CandyFansViewController.h
+//  JJImagePickerDemo
+//
+//  Created by shenjie on 2019/3/27.
+//  Copyright © 2019年 shenjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CandyFansViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JJImagePickerDemo/JJImagePickerDemo/ImagePicker/Views/Home/CandyFansViewController.m
@@ -0,0 +1,32 @@
+//
+//  CandyFansViewController.m
+//  JJImagePickerDemo
+//
+//  Created by shenjie on 2019/3/27.
+//  Copyright © 2019年 shenjie. All rights reserved.
+//
+
+#import "CandyFansViewController.h"
+
+@interface CandyFansViewController ()
+
+@end
+
+@implementation CandyFansViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
