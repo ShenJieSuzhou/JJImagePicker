85993832021f199de9816375034f6d5029fe1cb1
ShenJieSuzhou <745689122@qq.com>
2019-06-05 13:43:48 +0800

    请求地址添加
    请求地址添加

--- JJImagePickerDemo/JJImagePickerDemo/ImagePicker/Util/HttpRequestUrlDefine.h
+++ JJImagePickerDemo/JJImagePickerDemo/ImagePicker/Util/HttpRequestUrlDefine.h
@@ -9,98 +9,118 @@
 #ifndef HttpRequestUrlDefine_h
 #define HttpRequestUrlDefine_h
     
-//#define AC_LOGIN_REQUEST                                @"http://www.candyart.top/login/accountLogin"
+#define AC_LOGIN_REQUEST                                @"http://www.candyart.top/login/accountLogin"
+
+#define REGISTER_USER_REQUEST                           @"http://www.candyart.top/login/registeUser"
+
+#define VERIFY_TOKEN_REQUEST                            @"http://www.candyart.top/login/tokenVerify"
+
+#define GET_WORKS_REQUEST                               @"http://www.candyart.top/admin/api/getMyWorks"
+
+#define POST_WORKS_REQUEST                              @"http://www.candyart.top/admin/api/postMyWorks"
+
+#define OTHERS_DATA_REQUEST                             @"http://www.candyart.top/admin/api/othersData"
+
+#define UPDATE_NICKNAME_REQUEST                         @"http://www.candyart.top/setting/api/updateNickName"
+
+#define UPDATE_GENDER_REQUEST                           @"http://www.candyart.top/setting/api/updateGender"
+
+#define UPDATE_BIRTH_REQUEST                            @"http://www.candyart.top/setting/api/updateUserBirth"
+
+#define UPDATE_AVATAR_REQUEST                           @"http://www.candyart.top/setting/api/updateUserAvatar"
+
+#define UPDATE_MY_PASSWORD                              @"http://www.candyart.top/setting/api/updatePwd"
+
+#define SET_NEWPWD_REQUEST                              @"http://www.candyart.top/setting/api/setNewPwd"
+
+#define BIND_PHONECODE_REQUEST                          @"http://www.candyart.top/setting/api/reqBindPhoneCode"
+
+#define BIND_PHONE_REQUEST                              @"http://www.candyart.top/setting/api/bindUserPhone"
+
+#define WECHAT_AUTHORIZATION                            @"http://www.candyart.top/login/weChatAuthorization"
+
+#define THIRDPLATFORM_LOGIN                             @"http://www.candyart.top/login/thirdPlatformLogin"
+
+#define HOT_DISCOVERY_REQUEST                           @"http://www.candyart.top/admin/hot/findAll"
+
+#define POST_LIKE_REQUEST                               @"http://www.candyart.top/admin/photo/like"
+
+#define POST_UNLIKE_REQUEST                             @"http://www.candyart.top/admin/photo/unlike"
+
+#define START_FOCUS_REQUEST                             @"http://www.candyart.top/admin/photo/focus"
+
+#define CANCEL_FOCUS_REQUEST                            @"http://www.candyart.top/admin/photo/unfocus"
+
+#define UPDATE_MY_PASSWORD                              @"http://www.candyart.top/setting/api/updatePwd"
+
+#define TIPOFF_REQUEST                                  @"http://www.candyart.top/admin/tipoff"
+
+#define PULL_BLACK_REQUEST                              @"http://www.candyart.top/admin/pullblack"
+
+#define RELEASE_BLOCK_REQUEST                           @"http://www.candyart.top/admin/releaseBlockUser"
+
+#define QUERY_BLACKLIST_REQUEST                         @"http://www.candyart.top/admin/queryBlacklist"
+
+
+
+
+
+//#define AC_LOGIN_REQUEST                                @"http://172.16.10.113:8080/login/accountLogin"
 //
-//#define REGISTER_USER_REQUEST                           @"http://www.candyart.top/login/registeUser"
+//#define REGISTER_USER_REQUEST                           @"http://172.16.10.113:8080/login/registeUser"
 //
-//#define VERIFY_TOKEN_REQUEST                            @"http://www.candyart.top/login/tokenVerify"
+//#define VERIFY_TOKEN_REQUEST                            @"http://172.16.10.113:8080/login/tokenVerify"
 //
-//#define GET_WORKS_REQUEST                               @"http://www.candyart.top/admin/api/getMyWorks"
+//#define GET_WORKS_REQUEST                               @"http://172.16.10.113:8080/admin/api/getMyWorks"
 //
-//#define POST_WORKS_REQUEST                              @"http://www.candyart.top/admin/api/postMyWorks"
+//#define POST_WORKS_REQUEST                              @"http://172.16.10.113:8080/admin/api/postMyWorks"
 //
-//#define OTHERS_DATA_REQUEST                             @"http://www.candyart.top/admin/api/othersData"
+//#define OTHERS_DATA_REQUEST                             @"http://172.16.10.113:8080/admin/api/othersData"
 //
-//#define UPDATE_NICKNAME_REQUEST                         @"http://www.candyart.top/setting/api/updateNickName"
+//#define UPDATE_NICKNAME_REQUEST                         @"http://172.16.10.113:8080/setting/api/updateNickName"
 //
-//#define UPDATE_GENDER_REQUEST                           @"http://www.candyart.top/setting/api/updateGender"
+//#define UPDATE_GENDER_REQUEST                           @"http://172.16.10.113:8080/setting/api/updateGender"
 //
-//#define UPDATE_BIRTH_REQUEST                            @"http://www.candyart.top/setting/api/updateUserBirth"
+//#define UPDATE_BIRTH_REQUEST                            @"http://172.16.10.113:8080/setting/api/updateUserBirth"
 //
-//#define UPDATE_AVATAR_REQUEST                           @"http://www.candyart.top/setting/api/updateUserAvatar"
+//#define UPDATE_AVATAR_REQUEST                           @"http://172.16.10.113:8080/setting/api/updateUserAvatar"
 //
-//#define UPDATE_MY_PASSWORD                              @"http://www.candyart.top/setting/api/updatePwd"
+//#define UPDATE_MY_PASSWORD                              @"http://172.16.10.113:8080/setting/api/updatePwd"
 //
-//#define SET_NEWPWD_REQUEST                              @"http://www.candyart.top/setting/api/setNewPwd"
+//#define SET_NEWPWD_REQUEST                              @"http://172.16.10.113:8080/setting/api/setNewPwd"
 //
-//#define BIND_PHONECODE_REQUEST                          @"http://www.candyart.top/setting/api/reqBindPhoneCode"
+//#define BIND_PHONECODE_REQUEST                          @"http://172.16.10.113:8080/setting/api/reqBindPhoneCode"
 //
-//#define BIND_PHONE_REQUEST                              @"http://www.candyart.top/setting/api/bindUserPhone"
+//#define BIND_PHONE_REQUEST                              @"http://172.16.10.113:8080/setting/api/bindUserPhone"
 //
-//#define WECHAT_AUTHORIZATION                            @"http://www.candyart.top/login/weChatAuthorization"
+//#define WECHAT_AUTHORIZATION                            @"http://172.16.10.113:8080/login/weChatAuthorization"
 //
-//#define THIRDPLATFORM_LOGIN                             @"http://www.candyart.top/login/thirdPlatformLogin"
+//#define THIRDPLATFORM_LOGIN                             @"http://172.16.10.113:8080/login/thirdPlatformLogin"
 //
-//#define HOT_DISCOVERY_REQUEST                           @"http://www.candyart.top/admin/hot/findAll"
+//#define HOT_DISCOVERY_REQUEST                           @"http://172.16.10.113:8080/admin/hot/findAll"
 //
-//#define POST_LIKE_REQUEST                               @"http://www.candyart.top/admin/photo/like"
+//#define POST_LIKE_REQUEST                               @"http://172.16.10.113:8080/admin/photo/like"
 //
-//#define POST_UNLIKE_REQUEST                             @"http://www.candyart.top/admin/photo/unlike"
+//#define POST_UNLIKE_REQUEST                             @"http://172.16.10.113:8080/admin/photo/unlike"
 //
-//#define START_FOCUS_REQUEST                             @"http://www.candyart.top/admin/photo/focus"
+//#define START_FOCUS_REQUEST                             @"http://172.16.10.113:8080/admin/photo/focus"
 //
-//#define CANCEL_FOCUS_REQUEST                            @"http://www.candyart.top/admin/photo/unfocus"
+//#define CANCEL_FOCUS_REQUEST                            @"http://172.16.10.113:8080/admin/photo/unfocus"
+//
+//#define TIPOFF_REQUEST                                  @"http://172.16.10.113:8080/admin/tipoff"
+//
+//#define PULL_BLACK_REQUEST                              @"http://172.16.10.113:8080/admin/pullblack"
+//
+//#define RELEASE_BLOCK_REQUEST                           @"http://172.16.10.113:8080/admin/releaseBlockUser"
+//
+//#define QUERY_BLACKLIST_REQUEST                         @"http://172.16.10.113:8080/admin/queryBlacklist"
+//
+//#define UPDATE_MY_PASSWORD                              @"http://172.16.10.113:8080/setting/api/updatePwd"
 
 
-#define AC_LOGIN_REQUEST                                @"http://172.16.10.113:8080/login/accountLogin"
 
-#define REGISTER_USER_REQUEST                           @"http://172.16.10.113:8080/login/registeUser"
 
-#define VERIFY_TOKEN_REQUEST                            @"http://172.16.10.113:8080/login/tokenVerify"
 
-#define GET_WORKS_REQUEST                               @"http://172.16.10.113:8080/admin/api/getMyWorks"
-
-#define POST_WORKS_REQUEST                              @"http://172.16.10.113:8080/admin/api/postMyWorks"
-
-#define OTHERS_DATA_REQUEST                             @"http://172.16.10.113:8080/admin/api/othersData"
-
-#define UPDATE_NICKNAME_REQUEST                         @"http://172.16.10.113:8080/setting/api/updateNickName"
-
-#define UPDATE_GENDER_REQUEST                           @"http://172.16.10.113:8080/setting/api/updateGender"
-
-#define UPDATE_BIRTH_REQUEST                            @"http://172.16.10.113:8080/setting/api/updateUserBirth"
-
-#define UPDATE_AVATAR_REQUEST                           @"http://172.16.10.113:8080/setting/api/updateUserAvatar"
-
-#define UPDATE_MY_PASSWORD                              @"http://172.16.10.113:8080/setting/api/updatePwd"
-
-#define SET_NEWPWD_REQUEST                              @"http://172.16.10.113:8080/setting/api/setNewPwd"
-
-#define BIND_PHONECODE_REQUEST                          @"http://172.16.10.113:8080/setting/api/reqBindPhoneCode"
-
-#define BIND_PHONE_REQUEST                              @"http://172.16.10.113:8080/setting/api/bindUserPhone"
-
-#define WECHAT_AUTHORIZATION                            @"http://172.16.10.113:8080/login/weChatAuthorization"
-
-#define THIRDPLATFORM_LOGIN                             @"http://172.16.10.113:8080/login/thirdPlatformLogin"
-
-#define HOT_DISCOVERY_REQUEST                           @"http://172.16.10.113:8080/admin/hot/findAll"
-
-#define POST_LIKE_REQUEST                               @"http://172.16.10.113:8080/admin/photo/like"
-
-#define POST_UNLIKE_REQUEST                             @"http://172.16.10.113:8080/admin/photo/unlike"
-
-#define START_FOCUS_REQUEST                             @"http://172.16.10.113:8080/admin/photo/focus"
-
-#define CANCEL_FOCUS_REQUEST                            @"http://172.16.10.113:8080/admin/photo/unfocus"
-
-#define TIPOFF_REQUEST                                  @"http://172.16.10.113:8080/admin/tipoff"
-
-#define PULL_BLACK_REQUEST                              @"http://172.16.10.113:8080/admin/pullblack"
-
-#define RELEASE_BLOCK_REQUEST                           @"http://172.16.10.113:8080/admin/releaseBlockUser"
-
-#define QUERY_BLACKLIST_REQUEST                         @"http://172.16.10.113:8080/admin/queryBlacklist"
 
 //#define AC_LOGIN_REQUEST                                @"http://192.168.31.87:8080/login/accountLogin"
 //
